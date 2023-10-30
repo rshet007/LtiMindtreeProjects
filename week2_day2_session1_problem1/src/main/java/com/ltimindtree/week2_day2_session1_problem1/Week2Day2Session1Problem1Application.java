2a253d42edc73366f6736e937e793c49a666fc90
= <=>
2023-10-30 13:01:45 +0530

    Rajesh - 30-10-2023 13:01

---
+++ week2_day2_session1_problem1/src/main/java/com/ltimindtree/week2_day2_session1_problem1/Week2Day2Session1Problem1Application.java
@@ -0,0 +1,13 @@
+package com.ltimindtree.week2_day2_session1_problem1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2Day2Session1Problem1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week2Day2Session1Problem1Application.class, args);
+	}
+
+}
---
+++ week2_day2_session1_problem1/src/test/java/com/ltimindtree/week2_day2_session1_problem1/Week2Day2Session1Problem1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ltimindtree.week2_day2_session1_problem1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2Day2Session1Problem1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
